460d5263c9431315cdefe788de0a0bedd14a1d65
plm2034 <52817763+plm2034@users.noreply.github.com>
2019-07-17 15:11:42 +0900

    Create 모범답안_큐.cpp

---
+++ 코테_부수기_1/1_1158_조세퍼스_문제/모범답안_큐.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,K,cnt;
+queue<int> que;
+int main() {
+  cnt=0;
+  cin >> N >> K;
+  for(int i=1; i<=N; i++) que.push(i);
+  cout << '<';
+  while(que.size()!=1) {
+    int t;
+    t=que.pop();
+    if(++cnt==K) {
+      cout << t << ", ";
+      cnt=0;
+    } else que.push(t);
+  }
+  cout << que.front() << '>';
+  return 0;
+}
