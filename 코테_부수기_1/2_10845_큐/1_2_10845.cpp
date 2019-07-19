78d36eac4390ab4461472a1223dad53f9172dfd2
remnantdochi <remnantyeji@gmail.com>
2019-07-19 18:36:22 +0900

    왜 죽는지 모르겠음 ㅠㅠ 완벽해보임...

--- 코테_부수기_1/2_10845_큐/1_2_10845.cpp
+++ 코테_부수기_1/2_10845_큐/1_2_10845.cpp
@@ -7,9 +7,74 @@ using namespace std;
 
 typedef struct node {
 	int number;
+	struct node* prev;
 	struct node* next;
 }NODE;
+typedef struct que {
+	struct node* front;
+	struct node* rear;
+	int quesize;
+}QUE;
+void pushing(int item, QUE* line) {
+	NODE* nNode = (NODE*)malloc(sizeof(NODE));
+	nNode->number = item;
+	nNode->prev = line->rear;
+	nNode->next = NULL;
+	if (line->front == NULL) { //Á© Ã· Ãß°¡ÇÒ¶§
+		line->front = nNode;
+		line->rear = line->front;
+	}
+	else { 
+		line->rear->prev = nNode;
+		line->rear = nNode; 
+	}
+	line->quesize++;
+}
+int poping(QUE* line) {
+	if (line->front == NULL) return -1;
+	int value = line->front->number;
+	NODE* dNode = line->front;
+	line->front = line->front->prev;
+	//free(dNode);
+	if (line->quesize == 1) line->rear = NULL; //1°³¸¸ ³²¾ÒÀ»¶§
+	line->quesize--;
+	return value;
+}
+int front(QUE* line) {
+	return line->front->number;
+}
+int back(QUE* line) {
+	return line->rear->number;
+}
+int size(QUE* line) {
+	return line->quesize;
+}
+int empty(QUE* line) {
+	if (line->quesize) return 0;
+	return 1;
+}
 
 void main() {
+	int N; //command °¹¼ö
+	cin >> N;
+	QUE* problem = (QUE*)malloc(sizeof(QUE));
+	problem->front = NULL;
+	problem->rear = NULL;
+	problem->quesize = 0;
+	for (int i = 0; i < N; i++) {
+		string command;
+		int number;
+		cin >> command;
+		if (command == "push") {
+			cin >> number;
+			pushing(number, problem);
+		}
+		else if (command == "pop") cout << poping(problem) << endl;
+		else if (command == "front") cout << front(problem) << endl;
+		else if (command == "back") cout << back(problem) << endl;
+		else if (command == "size") cout << size(problem) << endl;
+		else cout << empty(problem) << endl;
 
+	}
+	free(problem);
 }
