416f147c8b6d9e3fb54a8586d08e24ec9e631ef6
remnantdochi <remnantyeji@gmail.com>
2019-07-21 20:19:19 +0900

    쉬벌탱 문제 정확히 묘사해라

--- 코테_부수기_2/2_1652_누울_자리를_찾아라/2_2_1652.cpp
+++ 코테_부수기_2/2_1652_누울_자리를_찾아라/2_2_1652.cpp
@@ -11,40 +11,46 @@ int main()
 	for (int i = 0;i < N;i++) {
 		cin >> map[i];
 		int cnt = 0;
-		int j = 0;
-		while (j < N) {
+		bool flag = false;
+		for (int j = 0; j < N;j++) {
 			if (map[i][j] == 'X') {
+				flag = false;
+				//cout << "test1 " << "i: " << i << " j: " << j << " cnt: " << cnt << endl;
 				cnt = 0;
-				//j++;
-				cout << "test1" << "i : " << i << " j : " << j << " cnt: " << cnt << endl;
-				j++;
 				continue;
 			}
 			if (++cnt == 2) {
-				row++;
-				cout << "test2" << "i : " << i << " j : " << j << " cnt: " << cnt << endl;
-				//j++;
-				break;
+				if (flag == false) {
+					flag = true;
+					row++;
+					//cout << "test2 " << "i: " << i << " j: " << j << " cnt: " << cnt << endl;
+				}
+				//cout << "test3 " << "i: " << i << " j: " << j << " cnt: " << cnt << endl;
+				cnt = 0;
 			}
-			cout << "test3" << "i : " << i << " j : " << j << " cnt: " << cnt << endl;
-			j++;
+			//cout << "test4 " << "i: " << i << " j: " << j << " cnt: " << cnt << "row : "<< row << endl;
 		}
 	}
-	cout << endl;
+	//cout << endl;
 	for (int i = 0;i < N;i++) {
 		int cnt = 0;
+		bool flag = false;
 		for (int j = 0; j < N;j++) {
 			if (map[j][i] == 'X') {
+				flag = false;
 				cnt = 0;
-				cout << "test4" << "i : " << i << " j : " << j << " cnt: " << cnt << endl;
+				//cout << "test4 " << "i: " << i << " j: " << j << " cnt: " << cnt << endl;
 				continue;
 			}
 			if (++cnt == 2) {
-				col++;
-				cout << "test5" << "i : " << i << " j : " << j << " cnt: " << cnt << endl;
-				break;
+				if (flag == false) {
+					flag = true;
+					col++;
+					//cout << "test5 " << "i: " << i << " j: " << j << " cnt: " << cnt << endl;
+				}
+				//cout << "test6 " << "i: " << i << " j: " << j << " cnt: " << cnt << endl;
+				cnt = 0;
 			}
-			cout << "test6" << "i : " << i << " j : " << j << " cnt: " << cnt << endl;
 		}
 	}
 
