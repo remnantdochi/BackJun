c067863804557bfc6a17f9e2a3d308a754217d58
remnantdochi <remnantyeji@gmail.com>
2019-08-17 23:09:30 +0900

    final

--- 코테_부수기_2/3_2621_카드게임/2_3_2621.py
+++ 코테_부수기_2/3_2621_카드게임/2_3_2621.py
@@ -33,15 +33,15 @@ elif 2 in dictn.values():
     if dictn[numbers[0]] ==2: #aa???
         if dictn[numbers[2]] == 2 and dictn[numbers[4]] == 1: #aabbc
             scores.append(numbers[2]*10 + numbers[0] + 300)
-        else : scores.append(numbers[0] + 200)
-    elif dictn[numbers[1]] == 2 :#abbcc
-        if dictn[numbers[0]] == 1 and dictn[numbers[3]] == 2:#abbcc
-            scores.append(numbers[3]*10 + numbers[1] + 300)
-        else: scores.append(numbers[1] + 200)
-    elif dictn[numbers[3]] == 2 : #aabcc
-        if dictn[numbers[0]] == 2 and dictn[numbers[2]] == 1 :
+        elif dictn[numbers[2]] == 1 and dictn[numbers[3]] == 2: #aabcc
             scores.append(numbers[3]*10 + numbers[0] + 300)
-        else : scores.append(numbers[3] + 200)
+        else : scores.append(numbers[0] + 200)
+    elif dictn[numbers[1]] == 2: #?bb
+        if dictn[numbers[4]] == 2:#abbcc
+            scores.append(numbers[4]*10 + numbers[1] + 300)
+        else : scores.append(numbers[1] + 200) #?bbcd
+    elif dictn[numbers[2]] == 2: scores.append(numbers[2]+200)#abccd
+    else: scores.append(numbers[3]+200)
 else : scores.append(100+numbers[4])
 
 print(max(scores))
