5ae05b6a74acfba3bbc869512180c869e6823144
remnantdochi <remnantyeji@gmail.com>
2019-08-17 21:42:49 +0900

    도움 ㅠㅠㅠ

--- 코테_부수기_2/3_2621_카드게임/2_3_2621.cpp
+++ 코테_부수기_2/3_2621_카드게임/2_3_2621.cpp
@@ -12,15 +12,14 @@ int main() {
 	}
 	sort(number, number + 5);
 	sort(card, card + 5);
-	//cout << card[4] << "card" << endl;
-	//cout << number[4] << "num" << endl;
+	cout << card[0] << card[1] << card[2] << card[3] << card[4] << "card" << endl;
 
-	if (card[0] == card[1] == card[2] == card[3] == card[4]) {
-		if (number[4] - 1 == number[3] && number[3] - 1 == number[2] && number[2] - 1 == number[1] && number[1] - 1 == number[0]) {
+	if ((card[0] == card[1]) && (card[1] == card[2]) && (card[2] == card[3]) && (card[3] == card[4])) {
+		if ((number[4] - 1 == number[3]) && (number[3] - 1 == number[2]) && (number[2] - 1 == number[1]) && (number[1] - 1 == number[0])) {
 			cout << number[4] + 900 << endl;
 		}
 	}
-	else if (((number[0] == number[1] == number[2] == number[3]) && (number[3] != number[4]) || (number[0] != number[1])  &&(number[1] == number[2] == number[3] == number[4]))) {
+	else if (((number[0] == number[1] && number[1] == number[2] && number[2]== number[3]) && (number[3] != number[4]) || (number[0] != number[1])  &&(number[1] == number[2] && number[2] == number[3] && number[3] == number[4]))) {
 		if (number[0] == number[1]) {
 			cout << number[0] + 800 << endl;
 		}
@@ -28,21 +27,21 @@ int main() {
 			cout << number[1] + 800 << endl;
 		}
 	}
-	else if ((number[0] == number[1]) && (number[2] == number[3] == number[4]) || (number[0] == number[1] == number[2]) && (number[3] == number[4]) ) {
-		if (number[0] == number[1] == number[2]) {
+	else if ((number[0] == number[1]) && (number[2] == number[3] && number[3] == number[4]) || (number[0] == number[1] && number[1] == number[2]) && (number[3] == number[4]) ) {
+		if (number[0] == number[1] && number[1] == number[2]) {
 			cout << number[0] * 10 + number[3] + 700 << endl;
 		}
 		else {
 			cout << number[0] + number[2] * 10 + 700 << endl;
 		}
 	}
-	else if (card[0] == card[1] == card[2] == card[3] == card[4]) {
+	else if (card[0] == card[1] && card[1] == card[2]  && card[2] == card[3] && card[3] == card[4]) {
 		cout << number[4] + 600 << endl;
 	}
 	else if (number[4] - 1 == number[3] && number[3] - 1 == number[2] && number[2] - 1 == number[1] && number[1] - 1 == number[0]) {
 		cout << number[4] + 500 << endl;
 	}
-	else if (number[0] == number[1] == number[2] || number[1] == number[2] == number[3] || number[2] == number[3] == number[4]) {
+	else if ((number[0] == number[1] && number[1] == number[2]) || (number[1] == number[2] && number[2] == number[3]) || (number[2] == number[3] && number[3] == number[4])) {
 		cout << number[2] + 400 << endl;
 	}
 	else if ((number[0] == number[1] && number[2] == number[3]) || (number[1] == number[2] && number[3] == number[4])) {
