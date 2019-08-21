3e15349df1f257873cf77df752a7c620eacd22bd
remnantdochi <remnantyeji@gmail.com>
2019-08-21 19:55:24 +0900

    같은거 미리 분류 후 걔네만 비교

---
+++ 코테_부수기_2/5_8979_올림픽/190821.py
@@ -0,0 +1,86 @@
+N, K = map(int, input().split())
+inputs = [[0]*4]*N
+order = [0]*N
+
+for i in range(N):
+    inputs[i] = list(map(int,input().split()))
+medals = list(zip(*inputs))
+
+# 일단 금메달부터
+gold = [medals[1],medals[0]]
+goldorder = list(zip(*gold))
+goldorder.sort(reverse = True)
+
+#print('gold',goldorder)
+
+goldsame = [] # >>> silver
+current = goldorder[0][0]
+currentsame = []
+currentnum = 0
+
+score = 0
+for item in goldorder:
+    if current == item[0]:
+        currentsame.append((medals[2][item[1]-1],item[1]))
+        order[item[1]-1] = score
+        currentnum +=1
+        #print('same',item,score,currentsame)
+    else :
+        if len(currentsame) != 1 : goldsame.append(currentsame)
+        currentsame = [(medals[2][item[1]-1],item[1])]
+        score += currentnum
+        order[item[1]-1] = score
+        currentnum = 1
+        current = item[0]
+        #print('not same', item, score, currentsame)
+if len(currentsame) != 1 : goldsame.append(currentsame)
+#print(order)
+#print(goldsame)
+
+#은메달 : 금메달에서 순위가 결정되지 않은 얘들만
+for silverorder in goldsame:
+
+    silverorder.sort(reverse = True)
+    #print('silver',silverorder)
+    silversame = []
+    current = silverorder[0][0]
+    currentsame = []
+    currentnum = 0
+    score = 0
+
+    for item in silverorder:
+        if current == item[0]:
+            currentsame.append((medals[3][item[1]-1],item[1]))
+            order[item[1]-1] += score
+            currentnum +=1
+            #print('same',item,score,currentsame)
+        else :
+            if len(currentsame) != 1 : silversame.append(currentsame)
+            currentsame = [(medals[3][item[1]-1],item[1])]
+            score += currentnum
+            order[item[1]-1] += score
+            currentnum = 1
+            current = item[0]
+            #print('not same', item, score, currentsame)
+if len(currentsame) != 1 : silversame.append(currentsame)
+#print('order',order)
+#print('silversame',silversame)
+
+for bronzeorder in silversame:
+    bronzeorder.sort(reverse= True)
+    current = bronzeorder[0][0]
+    currentnum = 0
+    score = 0
+    for item in bronzeorder:
+        if current == item[0]:
+            order[item[1]-1] += score
+            currentnum +=1
+            #print('same',item,score,currentsame)
+        else :
+            score += currentnum
+            order[item[1]-1] += score
+            currentnum = 1
+            current = item[0]
+            #print('not same', item, score, currentsame)
+
+print(order[K-1]+1)
